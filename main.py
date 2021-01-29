d0c53e1277eb1e8a332c2f0e0ac66ef7d62da75f
amurakho <amurakho@gmail.com>
2021-01-29 19:41:36 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import math
+
+
+value2 = [
+    1,
+    2,
+    3,
+    4
+]
+value2 = "[1, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3," \
+         " 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4]"
+value3 = pow(2, 10)
+
+if value2 == [] \
+        and value2 == []\
+        and value2 == []\
+        and value2 == []\
+        and value2 == []\
+        and value2 == []and value2 == []and value2 == []and value2 == []:
+    pass
+
+tuple1 = (1, )  # without whitespace
+tuple2 = (1,)  # without whitespace
+
+result1 = '1234567'[1:3]
+result2 = '1234567'[1:3]
+result3 = '1234567'[1 + tuple1[0]:5 + 10]
+
+
+def test_function(argument):
+    return argument[:10]
+
+
+if value4['t'] == 1:
+    test_function('*' * 10)
+
+
+test_function('0' * 10); test_function('+' * 10); test_function('-' * 10)
+
+
+values = [1, 902, 312, 431, 521, 619, 722, 8209, 912]
+
+
+def foo(x):
+    if x >= 0:
+        return math.sqrt(x)
+
+
+def bar(x):
+    if x < 0:
+        return
+    return math.sqrt(x)
